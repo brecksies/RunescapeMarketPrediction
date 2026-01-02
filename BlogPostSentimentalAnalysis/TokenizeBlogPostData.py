504d55f3b05d961e93285c6547c75715df692702
brecksies <brecksie@gmail.com>
2026-01-01 18:57:52 -0700

    Added in tokenizing function.

--- BlogPostSentimentalAnalysis/TokenizeBlogPostData.py
+++ BlogPostSentimentalAnalysis/TokenizeBlogPostData.py
@@ -1,5 +1,6 @@
 from tokenizers import Tokenizer
 from transformers import AutoTokenizer
+from pathlib import Path
 import torch
 
 def tokenize_texts(texts, model_name='bert-base-uncased', max_length=4096):
@@ -17,3 +18,15 @@ def tokenize_texts(texts, model_name='bert-base-uncased', max_length=4096):
         'input_ids': encoded_inputs['input_ids'],
         'attention_mask': encoded_inputs['attention_mask']
     }
+
+def create_tokenized_blog_posts():
+    path = Path("BlogPostSentimentalAnalysis/BlogPosts/")
+
+    for file in path.iterdir():
+        if file.is_file() and file.suffix == ".txt":
+            with open(file, "r", encoding="utf-8") as f:
+                content = f.read()
+            tokenized_data = tokenize_texts([content])
+            torch.save(tokenized_data, file.with_suffix(".pt"))
+
+create_tokenized_blog_posts()
