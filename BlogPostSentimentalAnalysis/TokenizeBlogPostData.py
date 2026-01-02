14fbb42da1137da4c337b70965133b35972dd281
brecksies <brecksie@gmail.com>
2026-01-01 18:18:52 -0700

    Added in tokenize text function

--- BlogPostSentimentalAnalysis/TokenizeBlogPostData.py
+++ BlogPostSentimentalAnalysis/TokenizeBlogPostData.py
@@ -1,1 +1,19 @@
 from tokenizers import Tokenizer
+from transformers import AutoTokenizer
+import torch
+
+def tokenize_texts(texts, model_name='bert-base-uncased', max_length=4096):
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    encoded_inputs = tokenizer(
+        texts,
+        padding=True,
+        truncation=True,
+        max_length=max_length,
+        return_tensors='pt'
+    )
+
+    return {
+        'input_ids': encoded_inputs['input_ids'],
+        'attention_mask': encoded_inputs['attention_mask']
+    }
